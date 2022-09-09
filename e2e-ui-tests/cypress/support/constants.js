e063753af6c341fb76bc9854318523e51b0489ef
Niron Perera <snironr@gmail.com>
2022-09-09 23:56:16 +0530

    Update constants.js

--- e2e-ui-tests/cypress/support/constants.js
+++ e2e-ui-tests/cypress/support/constants.js
@@ -3,4 +3,4 @@ export const USERNAME = "userName";
 export const PASSWORD = "password";
 export const TIMEOUT_SHORT = 5000;
 export const TIMEOUT_MEDIUM = 10000;
-export const TIMEOUT_LONG = 15000;
+export const TIMEOUT_LONG = 25000;
