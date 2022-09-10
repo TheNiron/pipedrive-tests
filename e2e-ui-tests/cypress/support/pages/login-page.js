b74d5b28c369d59ecd9064e7923a71cf9a89ee5a
Niron Perera <snironr@gmail.com>
2022-09-10 13:30:13 +0530

    Update the failing tests with the new attributes. "data-test" attributes have been removed for some reason.

--- e2e-ui-tests/cypress/support/pages/login-page.js
+++ e2e-ui-tests/cypress/support/pages/login-page.js
@@ -1,23 +1,20 @@
 /// <reference types="cypress" />
 
-let nameField = "[name='name']";
-
-
 export class LoginPage {
 
-    static login(username,password) {
-        cy.get('[class="auth-form__title"]').contains("Log in");
-        cy.get('[data-test="login"]').type(username);
-        cy.get('[data-test="password"]').type(password);
-        cy.get('[data-test="submit-button"]').click();
+    static login(username, password) {
+        cy.get('[class="auth-form__container"]>h3').contains("Log in");
+        cy.get('[name="login"]').type(username);
+        cy.get('[name="password"]').type(password);
+        cy.get('[class="login"]>button').click();
         LoginPage.verifyAccountMenuButton;
-        }
+    }
 
     static verifyAccountMenuButton() {
         cy.get('[data-test="account-menu-button"]').should('be.visible');
-            }
+    }
 
 
-        
-  }
+
+}
 
